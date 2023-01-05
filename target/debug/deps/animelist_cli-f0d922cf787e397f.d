3c32dc37bafee98d482327b7629d2469e61b8c9f
oererenn <onur.ereren998@gmail.com>
2023-01-05 23:40:33 +0300

    added gitignore

--- target/debug/deps/animelist_cli-f0d922cf787e397f.d
+++
@@ -1,5 +0,0 @@
-/Users/onurereren/MyGit/animelist-cli/target/debug/deps/animelist_cli-f0d922cf787e397f.rmeta: src/main.rs
-
-/Users/onurereren/MyGit/animelist-cli/target/debug/deps/animelist_cli-f0d922cf787e397f.d: src/main.rs
-
-src/main.rs:
