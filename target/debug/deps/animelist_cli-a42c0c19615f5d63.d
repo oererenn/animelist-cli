cdbc84ee2df7003ed09919e99839b4ce4dbed7ec
oererenn <onur.ereren998@gmail.com>
2023-01-05 23:35:19 +0300

    Initialized the project.

---
+++ target/debug/deps/animelist_cli-a42c0c19615f5d63.d
@@ -0,0 +1,5 @@
+/Users/onurereren/MyGit/animelist-cli/target/debug/deps/animelist_cli-a42c0c19615f5d63.rmeta: src/main.rs
+
+/Users/onurereren/MyGit/animelist-cli/target/debug/deps/animelist_cli-a42c0c19615f5d63.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/animelist_cli-f0d922cf787e397f.d
@@ -0,0 +1,5 @@
+/Users/onurereren/MyGit/animelist-cli/target/debug/deps/animelist_cli-f0d922cf787e397f.rmeta: src/main.rs
+
+/Users/onurereren/MyGit/animelist-cli/target/debug/deps/animelist_cli-f0d922cf787e397f.d: src/main.rs
+
+src/main.rs:
