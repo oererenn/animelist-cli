49b80f66e6c2bb206f07b5e0e2b0ed0fc1c7e8f8
Onur Ereren <onur.ereren998@gmail.com>
2023-01-22 13:01:23 +0100

    readability improvements

--- src/main.rs
+++ src/main.rs
@@ -5,24 +5,8 @@ use args::Cli;
 use args::Commands;
 
 fn main() {
-
-    let top = reqwest::blocking::get("https://myanimelist.net/topanime.php")
-        .unwrap()
-        .text()
-        .unwrap();
-
-    let top_anime_document = scraper::Html::parse_fragment(&top);
-
-    let title_selector = scraper::Selector::parse("h3.hoverinfo_trigger").unwrap();
-
-    let mut v = Vec::new();
-    for x in top_anime_document.select(&title_selector){
-        let x = x.text().collect::<Vec<_>>();
-        v.push(x[0]);
-    }
-
     let _args = Cli::parse();
-
+    let v = get_top_anime();
     match &_args.command {
         Commands::TopTen(_top_ten) => {
             println!("Top Ten Anime");
@@ -38,5 +22,23 @@ fn main() {
         },
         Commands::TopAiring(_) => {}
         Commands::TopUpcoming(_) => {}
+    }
+
+    fn get_top_anime() -> Vec<String> {
+        let top = reqwest::blocking::get("https://myanimelist.net/topanime.php")
+            .unwrap()
+            .text()
+            .unwrap();
+
+        let top_anime_document = scraper::Html::parse_fragment(&top);
+
+        let title_selector = scraper::Selector::parse("h3.hoverinfo_trigger").unwrap();
+
+        let mut v: Vec<String> = Vec::new();
+        for x in top_anime_document.select(&title_selector){
+            let x = x.text().collect::<Vec<_>>();
+            v.push(x[0].to_string());
+        }
+        v
     }
 }
